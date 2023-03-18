866fed796a0a6773bb1362d1a278d5d5dbf2e7f5
OkruzhkoVS <OkruzhkoVS@411D-3>
2023-03-18 12:06:04 +0700

    Добавьте файлы проекта.

---
+++ LinkedList/MyList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class MyList<T> : IEnumerable<T>
+    {
+        Node<T>? head; 
+        Node<T>? tail; 
+        int count;
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+                tail!.Next = node;
+            tail = node;
+
+            count++;
+        }
+        public bool Remove(T data)
+        {
+            Node<T>? current = head;
+            Node<T>? previous = null;
+
+            while (current != null && current.Data != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        // убираем узел current, теперь previous ссылается не на current, а на current.Next
+                        previous.Next = current.Next;
+
+                        // Если current.Next не установлен, значит узел последний,
+                        // изменяем переменную tail
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        // если удаляется первый элемент
+                        // переустанавливаем значение head
+                        head = head?.Next;
+
+                        // если после удаления список пуст, сбрасываем tail
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        // содержит ли список элемент
+        public bool Contains(T data)
+        {
+            Node<T>? current = head;
+            while (current != null && current.Data != null)
+            {
+                if (current.Data.Equals(data)) return true;
+                current = current.Next;
+            }
+            return false;
+        }
+        // добвление в начало
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T>? current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        // реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+    }
+
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T>? Next { get; set; }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Выбирете что хотите сделать:\n" +
+                "Добавить элемент - 1\n" +
+                "Удалить элемент - 2\n" +
+                "Добавить в начало элемент - 3\n" +
+                "Вывести все элементы - 4\n" +
+                "Проверить наличие элемента - 5\n" +
+                "Выйти из программы - 6");
+                int operation = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                switch (operation)
+                {
+                    case 1:
+                        Console.WriteLine("Введите число, которое хотите добавить: ");
+                        int num = int.Parse(Console.ReadLine());
+                        list.Add(num);
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите число, которое хотите удалить: ");
+                        int num1 = int.Parse(Console.ReadLine());
+                        list.Remove(num1);
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите число, которое хотите добавить в начало: ");
+                        int num2 = int.Parse(Console.ReadLine());
+                        list.Add(num2);
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        if (list.IsEmpty == false)
+                        {
+                            Console.WriteLine("Вот все элементы списка: ");
+                            foreach (var item in list)
+                            {
+                                Console.WriteLine(item);
+                            }
+                            Console.WriteLine();
+                        }
+                        else Console.WriteLine("В списке нет элементов.");
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("Введите число, которое хотите проверить в наличии списка: : ");
+                        int num3 = int.Parse(Console.ReadLine());
+                        if (list.Contains(num3))
+                        {
+                            Console.WriteLine("Да, он есть.");
+                        }
+                        else Console.WriteLine("Такого элемента нет.");
+                        Console.WriteLine();
+                        break;
+                    case 6:
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+            
+    }
+}
